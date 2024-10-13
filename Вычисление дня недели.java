b4608cec1e7206f46a874a048084275a189c49d6
ventilia <116442238+ventilia@users.noreply.github.com>
2024-10-13 23:17:51 +0300

    Create Вычисление дня недели.java

---
+++ Вычисление дня недели.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        String data = scanner.nextLine();
+
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d M yyyy");
+        try {
+            LocalDate date = LocalDate.parse(data, formatter);
+
+
+            String res = date.format(DateTimeFormatter.ofPattern("EEEE", Locale.ENGLISH));
+            System.out.println(res);
+        } catch (DateTimeParseException e) {
+            return;
+        }
+
+    }
+}
+//код выглядит страшно и не понятно. я знаю.
+//это задание было у нас на прошлом на курсе. у нас была тема с работой над датами.
+//ссылки на библиотеки с которыми я работал https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+// этого в коде не было, но тож на всякий https://docs.oracle.com/javase/8/docs/api/java/text/SimpleDateFormat.html
+//локаль тут нужно для перевода с ру на англ.
+//такую сказку гпт бы не придумал точно.

cf44634d11bccbd293d83b143b09d4067b7dcbfb
ventilia <116442238+ventilia@users.noreply.github.com>
2024-10-13 21:25:45 +0300

    Update Число в обратном порядке.java

--- Число в обратном порядке.java
+++ Число в обратном порядке.java
@@ -1,1 +1,23 @@
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+
+        int res = 0;
+        while(number != 0) {
+            res = res * 10 + number % 10;
+            number /= 10;
+        }
+        System.out.println(res);
+
+        //ну я думаю гпт бы посложнее сделал
+            }
+        }
+
+
+
+
